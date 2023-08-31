9df4ba520149bb598069d48b16b619f2433cb89e
jeon <dasties@naver.com>
2023-08-31 15:21:51 +0900

    19강

--- main.js
+++ main.js
@@ -5,36 +5,42 @@ var url= require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData=url.parse(_url,true).query;
+    var pathname= url.parse(_url,true).pathname
     var title=queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title='welcome'
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+    console.log()
+   if(pathname === '/'){
+
+      fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+        var template = `<!doctype html>
+      <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description} </p>
+        </body>
+      </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+      });
+   }else{
+
+    response.writeHead(404);
+    response.end('not found');
+   }
+    
+    
     //response.end(fs.readFileSync(__dirname + _url));
-    var template = `<!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>
-        Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language. Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications.
-      </p>
-    </body>
-    </html>
-    `
-    response.end(template);
+    
+   
 });
 app.listen(3000);
